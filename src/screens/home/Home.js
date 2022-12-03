801e5b7f9606cfa454e05cb4d883113be0532435
Sakshijain12 <jain.60@iitj.ac.in>
2022-12-03 12:46:08 +0530

    home page added

---
+++ src/screens/home/Home.js
@@ -0,0 +1,23 @@
+import './Home.css';
+import React from 'react';
+
+function Home() {
+  return (
+    <body>
+      <div className='head'>
+        <a>Login</a>
+        <a>Sign up</a>
+      </div>
+      <div>
+        <img src = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"></img>
+      </div>
+      <div>
+        <p>This is an application used for ideation phase of an project, which is used by thinkers to ideate over an idea and to innovate new thoughts.</p>
+        <p>Disclaimer -- Site under progress</p>
+        <p>Made with love -- Admins</p>
+      </div>
+    </body>
+  );
+}
+
+export default Home;
